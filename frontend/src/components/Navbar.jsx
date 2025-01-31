2fd3dba9c4c834cfe26313cd032da3d3845168c8
Daniela Guardia <138531935+DaniJGP@users.noreply.github.com>
2025-01-31 16:44:52 -0300

    Agrega estilos para la vista de escritorio al navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,28 +2,43 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <img src="../assets/images/logo.png" alt="Galería de Arte" className="logo" />
-      <ul className="nav-links">
-        <li>
-          <a href="/">Inicio</a>
-        </li>
-        <li>
-          <a href="/gallery">Artistas</a>
-        </li>
-        <li>
-          <a href="/cart">Carrito</a>
-        </li>
-        <li>
-          <a href="/login">Iniciar sesión</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-primary" type="submit">
-          Search
-        </button>
-      </form>
+    <nav className="navbar navbar-expand" data-bs-theme="dark">
+      <div className="container">
+        <div className="navbar-brand">
+          <img src="../assets/images/logo.png" alt="Galería de Arte" />
+        </div>
+        <div class="navbar-nav ms-3 me-auto">
+          <a class="nav-link" href="#">
+            Login
+          </a>
+          <a class="nav-link" href="#">
+            Registro
+          </a>
+          <a class="nav-link" href="#">
+            Tienda
+          </a>
+        </div>
+        <div className="navbar-nav">
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Buscar"
+              aria-label="Search"
+              data-bs-theme="light"
+            />
+            <button class="btn btn-dark" type="submit">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </form>
+          <a href="#" className="nav-link">
+            <i class="fa-solid fa-user fa-lg"></i>
+          </a>
+          <a href="#" className="nav-link">
+            <i class="fa-solid fa-cart-shopping fa-lg"></i>
+          </a>
+        </div>
+      </div>
     </nav>
   );
 };
