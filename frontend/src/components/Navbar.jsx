06e80ced4c674609fade51d9c1fafe05de38bbbb
Daniela Guardia <138531935+DaniJGP@users.noreply.github.com>
2025-01-31 16:19:04 -0300

    Reorganizar archivos css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import '../App.css';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <img src="../assets/images/logo.png" alt="Galería de Arte" className="logo" />
       <ul className="nav-links">
-        <li><a href="/">Inicio</a></li>
-        <li><a href="/gallery">Artistas</a></li>
-        <li><a href="/cart">Carrito</a></li>
-        <li><a href="/login">Iniciar sesión</a></li>
+        <li>
+          <a href="/">Inicio</a>
+        </li>
+        <li>
+          <a href="/gallery">Artistas</a>
+        </li>
+        <li>
+          <a href="/cart">Carrito</a>
+        </li>
+        <li>
+          <a href="/login">Iniciar sesión</a>
+        </li>
       </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-primary" type="submit">
+          Search
+        </button>
+      </form>
     </nav>
   );
 };
