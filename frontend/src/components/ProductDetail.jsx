89a2e115edb3c9efea36db65caf1ac9a58e72a15
Daniela Guardia <138531935+DaniJGP@users.noreply.github.com>
2025-02-03 20:41:33 -0300

    Agrega imágenes, añade hooks a ProductDetail para futuras llamadas a API

--- frontend/src/components/ProductDetail.jsx
+++ frontend/src/components/ProductDetail.jsx
@@ -1,36 +1,38 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
 import './ProductDetail.css';
+import { useEffect, useState } from 'react';
+import { getProductById } from '../helpers/productsAPI';
 
 const ProductDetail = () => {
   const { id } = useParams();
   const { dispatch } = useCart();
   const navigate = useNavigate(); // Hook para la navegación
-
-  const product = {
-    id,
-    title: `Obra ${id}`,
-    author: `Autor ${id}`,
-    price: `$${id}00`,
-    image: `/images/obra${id}.jpg`,
-    description: `Descripción detallada de la obra ${id}.`,
-  };
+  const [product, setProduct] = useState(null);
 
   const handleAddToCart = () => {
     dispatch({ type: 'ADD_TO_CART', payload: product });
     navigate('/cart'); // Redirigir al carrito
   };
 
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const fetchedProduct = getProductById(id);
+      setProduct(fetchedProduct);
+    };
+    fetchProduct();
+  }, [id]);
+
   return (
     <div className="min-view-height d-flex justify-content-center align-items-center gap-5">
       <div>
-        <img className="product-detail-img" src={product.image} alt={product.title} />
+        <img className="product-detail-img" src={product.img_url} alt={product.title} />
       </div>
       <div className="product-detail text-center">
-        <h2>{product.title}</h2>
-        <p>{product.author}</p>
-        <p>{product.price}</p>
-        <p>{product.description}</p>
+        <h2>{product.nombre}</h2>
+        <p>{product.autor}</p>
+        <p>{product.precio}</p>
+        <p>{product.descripcion}</p>
         <button onClick={handleAddToCart} className="btn btn-primary">
           Añadir al Carrito
         </button>
